819c7c0148b02b013a937392c1ef412172543547
The Dark Eye <bit.bytes@outlook.com>
2022-06-16 11:11:55 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aliensoft.fireBase',
+  appName: 'fireBase',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
